6793007dacd3a6a3c63533a49f4b91adb7993da1
reejit <70316178+reejit@users.noreply.github.com>
2022-02-26 11:36:26 +0530

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -13,26 +13,26 @@ admin_filter=filters.create(is_admin)
 
 @bot.on_message(filters.command(["download"]) & chat_filter)
 async def Upload(bot, message):
-     chat_id = message.chat.id
+     
      if len(message.command) == 1:
-        await message.reply_text("No link!")
+        await message.reply("No link!")
         return
      link = message.command[1]     
      download = SmartDL(link, progress_bar=False)
-     m = message.reply_text("Downloading")
+     m = message.reply("Downloading")
      download.start(blocking=False)
      while not download.isFinished():
       Eta = download.get_eta(human=True)
       Progress = download.get_progress()*100)
       Bar = download.get_progress_bar())
-      m.edit_text(f"Downloading...\nEta: {Eta}\n\n{Bar}Progress: {Progress}")       
+      m.edit(f"Downloading...\nEta: {Eta}\n\n{Bar}Progress: {Progress}")       
       time.sleep(5)
      if download.isSuccessful():
-      m.edit_text("DOWNLOAD SUCCESSFUL")
+      m.edit("DOWNLOAD SUCCESSFUL")
      else:
       for e in download.get_errors():
-            m.edit_text(f"Errors\n\n\n{e}")
+            m.edit(f"Errors\n\n\n{e}")
      video = download.get_dest()
-     await m.edit_text("Uploading to you")
+     await m.edit("Uploading to you")
      await client.send_video(chat_id=chat_id, video=video)
      os.remove(video)
