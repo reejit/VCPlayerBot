f0a7f82b4d991e32b4e3f1d54787aab3618e7d88
reejit <70316178+reejit@users.noreply.github.com>
2022-02-26 11:29:12 +0530

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -16,22 +16,22 @@ async def Upload(client: Client, message: Message):
      if len(message.command) == 1:
         await message.reply_text("No link!")
         return
+     link = message.command[1]     
+     download = SmartDL(link, progress_bar=False)
+     m = message.reply_text("Downloading")
+     download.start(blocking=False)
+     while not download.isFinished():
+      Eta = download.get_eta(human=True)
+      Progress = download.get_progress()*100)
+      Bar = download.get_progress_bar())
+      m.edit_text(f"Downloading...\nEta: {Eta}\n\n{Bar}Progress: {Progress}")       
+      time.sleep(5)
+     if download.isSuccessful():
+      m.edit_text("DOWNLOAD SUCCESSFUL")
      else:
-       link = message.command[1]     
-       download = SmartDL(link, progress_bar=False)
-       m = message.reply_text("Downloading")
-       download.start(blocking=False)
-       while not download.isFinished():
-        Eta = download.get_eta(human=True)
-        Progress = download.get_progress()*100)
-        Bar = download.get_progress_bar())
-        m.edit_text(f"Downloading...\nEta: {Eta}\n\n{Bar}Progress: {Progress}")       
-        time.sleep(5)
-       if download.isSuccessful():
-        m.edit_text("DOWNLOAD SUCCESSFUL")
-       else:
-        for e in download.get_errors():
-              m.edit_text(f"Errors\n\n\n{e}")
-       video = download.get_dest()
-       await client.send_video(chat_id=chat_id, video=video)
-       os.remove(video)
+      for e in download.get_errors():
+            m.edit_text(f"Errors\n\n\n{e}")
+     video = download.get_dest()
+     await m.edit_text("Uploading to you")
+     await client.send_video(chat_id=chat_id, video=video)
+     os.remove(video)
